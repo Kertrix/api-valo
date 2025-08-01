21beaf0315a80b7208542b403adb84b82c4db2c9
Tristan Kermorvant <tristan.kermorvant@ejm.org>
2025-08-01 20:14:51 +0200

    refactored

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const express = require("express");
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
 const app = express();
 const port = 8000;
 
-const HenrikDevValorantAPI = require("unofficial-valorant-api");
-const vapi = new HenrikDevValorantAPI();
+let ids = { "toto": "9c02ea8a-5e35-554d-a897-4119aec05228" }
 
-app.get("/", async (req, res) => {
-  const mmr_data = await vapi.getMMR({
-    version: "v1",
-    region: "eu",
-    name: "WLyubaw",
-    tag: "EUW",
+async function getMMR(puuid) {
+  const response = await fetch(`https://api.henrikdev.xyz/valorant/v3/by-puuid/mmr/eu/pc/${puuid}`, {
+    method: 'GET',
+    headers: {
+      "Authorization": process.env.API_KEY,
+      "Accept": "*/*"
+    },
   });
+
+  return await response.json();
+}
+
+app.get("/toto", async (req, res) => {
+  const mmr_data = await getMMR(ids.toto);
   if (mmr_data.error) {
     res.send(`Error ${mmr_data.status}`);
-    return console.log(`Username: ${name}#${tag} Error: ${mmr_data.status}`);
+    return console.log(`ID: ${ids.toto} Error: ${mmr_data.status}`);
   }
-  res.send(`[${mmr_data.data.currenttierpatched}]: ${mmr_data.data.ranking_in_tier}RR`);
-});
+  res.send(`[${mmr_data.data.current.tier.name}]: ${mmr_data.data.current.rr}RR (${mmr_data.data.current.last_change})`);
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Valorant rank api app listening at http://localhost:${port}`);
 });
